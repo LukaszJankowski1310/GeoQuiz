4dac4dcac726437860e9e2c61e44f456ae8470b8
Lukasz Jankowski <lukasz.jankowski1310@gmail.com>
2022-06-18 19:41:06 +0200

    1st Chapter

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton : Button
+    private lateinit var falseButton : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {view : View ->
+           showToast(R.string.correct_toast)
+        }
+
+        falseButton.setOnClickListener {view : View ->
+            showToast(R.string.incorrect_toast)
+        }
+
     }
+
+    private fun showToast(message : Int) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
+    }
+
 }
