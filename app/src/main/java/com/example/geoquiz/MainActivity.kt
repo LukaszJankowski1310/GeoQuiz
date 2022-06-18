8701edf54ba2f75d7d2f0299117c44d56872b719
Lukasz Jankowski <lukasz.jankowski1310@gmail.com>
2022-06-19 00:54:36 +0200

    2nd Chapter - Model-View-Controler

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,29 +14,66 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton : Button
     private lateinit var falseButton : Button
+    private lateinit var nextButton : ImageButton
+    private lateinit var prevButton : ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+    private var currentIndex = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // vies
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
+        questionTextView = findViewById(R.id.question_text_view)
+        // listeners
         trueButton.setOnClickListener {view : View ->
-           showToast(R.string.correct_toast)
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener {view : View ->
-            showToast(R.string.incorrect_toast)
+            checkAnswer(false)
         }
 
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener {
+            if (currentIndex == 0) currentIndex = questionBank.size
+            currentIndex -= 1
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
     }
 
-    private fun showToast(message : Int) {
-        val toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
-        toast.setGravity(Gravity.CENTER, 0, 0)
-        toast.show()
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer : Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) R.string.correct_toast
+                           else R.string.incorrect_toast
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 
 }
